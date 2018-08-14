91d5243d4c5afbd832c5be3c574c6cc1257dc63a
Collins Liyosi <collinskivale@gmail.com>
2018-08-14 17:23:15 +0300

    Intialize project - Beans from spring application context

---
+++ src/main/java/com/liyosi/didemo/controllers/MyController.java
@@ -0,0 +1,14 @@
+package com.liyosi.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+@Controller
+public class MyController {
+
+  public void hello() {
+    System.out.println("Hello !!");
+  }
+}
